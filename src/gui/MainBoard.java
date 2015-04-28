fe4f389164e4ce199aef6420f4487738fa75eec3
Kent Johnson <kentoj@gmail.com>
2015-04-27 23:45:57 -0600

    added array lists of button references

--- src/gui/MainBoard.java
+++ src/gui/MainBoard.java
@@ -7,13 +7,18 @@
 package gui;
 
 import java.awt.Dimension;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainBoard extends JPanel {
 	Dimension sidePnls = new Dimension(200, 950);
 	Dimension topBottomPnls = new Dimension(1360, 205);
 	Dimension middle = new Dimension(950, 950);
+	ArrayList<JButton> topBoardButtons, leftButtons, rightButtons,
+			bottomBoardButtons;
+	Map<Integer,JButton> orderedButtons;
 	
 	/**
 	 * Create the panel.
@@ -38,10 +43,19 @@ public class MainBoard extends JPanel {
 		rSide.setPreferredSize(sidePnls);
 		add(rSide);
 		
-		JPanel bottom = new BottomBoard();
+		BottomBoard bottom = new BottomBoard();
 		bottom.setPreferredSize(topBottomPnls);
-		bottom.get
+		bottomBoardButtons = bottom.getAllProperties();
 		add(bottom);
+
+		orderedButtons = this.getOrderedListOfButtons();
+	}
+
+	private Map<Integer, JButton> getOrderedListOfButtons() {
+
+
+
+		return null;
 	}
 
 }
